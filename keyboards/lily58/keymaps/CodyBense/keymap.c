25d32feb80b745595334ce808a8748c6e3c43426
CodyBense <codybense@gmail.com>
2025-07-05 13:18:16 -0400

    lily58, updated keymap

--- keyboards/lily58/keymaps/CodyBense/keymap.c
+++ keyboards/lily58/keymaps/CodyBense/keymap.c
@@ -36,22 +36,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  | BSPC |
+ * |      |      |      |      |      |      |                    | PLUS |   7  |   8  |   9  |   [  | BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | PIPE |   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |      |
+ * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------| MINS |   4  |   5  |   6  |   ]  |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   +  |   -  |   =  |      |   \  |      |
+ * |      |   ^  |   &  |   *  |   (  |   )  |-------|    |-------|   =  |   1  |   2  |   3  |   0  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | LGUI |      | /       /       \      \  |      | RGUI |      |
+ *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_PIPE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS, KC_MINS, KC_EQL, XXXXXXX,  KC_BSLS, XXXXXXX,
-                             XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI, XXXXXXX
+  KC_F1,KC_F2,          KC_F3,        KC_F4,          KC_F5,          KC_F6,                   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_NO,KC_NO,          KC_NO,        KC_NO,          KC_NO,          KC_NO,                   KC_PLUS, KC_7,    KC_8,    KC_9,    KC_LBRC, KC_BSPC,
+  KC_NO,LGUI_T(KC_EXLM),LALT_T(KC_AT),LSFT_T(KC_HASH),LCTL_T(KC_DLR), KC_NO,                   KC_MINS, KC_4,    KC_5,    KC_6,    KC_RBRC, KC_NO,
+  KC_NO,KC_CIRC,        KC_AMPR,      KC_ASTR,        KC_LPRN,        KC_RPRN,  KC_NO, KC_NO,  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_0,    KC_BSLS,
+                        KC_NO,        KC_NO,          KC_NO,                    KC_NO, KC_NO,  KC_NO,   KC_NO,   KC_NO
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.

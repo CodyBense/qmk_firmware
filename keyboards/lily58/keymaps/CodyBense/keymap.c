de64e1183b42e984ca481503490e3ea99e234355
CodyBense <codybense@gmail.com>
2025-07-10 22:42:27 -0400

    lily58, updated keymap, fixed volume control

--- keyboards/lily58/keymaps/CodyBense/keymap.c
+++ keyboards/lily58/keymaps/CodyBense/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  LGUI_T(KC_A),   LALT_T(KC_S),    LSFT_T(KC_D),    LCTL_T(KC_F),    KC_G,                     KC_H,    RCTL_T(KC_J),    RSFT_T(KC_K),    RALT_T(KC_L),    RGUI_T(KC_SCLN), KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_VOLU,  KC_VOLD,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_KB_VOLUME_DOWN,  KC_KB_VOLUME_UP,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RGUI, KC_BSPC
 ),
 /* LOWER
